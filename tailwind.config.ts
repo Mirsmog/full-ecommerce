f8cbc06b2c8d18b9ab2517e85491a8aadf6a5e73
zwine <tanharka.tyuchiev4@mail.ru>
2024-09-05 23:05:44 +0500

    feat: update Tailwind configuration
    - Added color palette to tailwind.config.js
    - Added font styles in @layer components
    - Configured container for responsive layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,51 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "0.75rem",
+      screens: {
+        xl: "1116px",
+        "2xl": "1116px",
+      },
+    },
     extend: {
+      boxShadow: {
+        sm: "2px 2px 30px rgba(0, 0, 0, 0.05);",
+        md: "4px 4px 4px rgba(0, 0, 0, 0.05);",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#4078FF",
+          800: "#5C83FF",
+          700: "#728FFF",
+          600: "869AFF",
+          500: "97A6FF",
+          400: "A8B2FF",
+          300: "B7BFFF",
+          200: "E3E5FF",
+          100: "F0F1FF",
+        },
+        neutral: {
+          black: {
+            DEFAULT: "#0E1422",
+            800: "#202533",
+            700: "#333845",
+            600: "#474B57",
+            500: "#5C5F6A",
+            400: "#71747E",
+            300: "#878A92",
+            200: "#B6B7BC",
+            100: "#E6E7E8",
+          },
+          white: {
+            DEFAULT: "#FFFFFF",
+            200: "#E9E9EB",
+            100: "#F6F6F6",
+          },
+        },
       },
     },
   },
